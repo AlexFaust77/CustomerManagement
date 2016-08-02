e93e8f5f9edb324de530f6dcffa0c0c5df9990c4
Breas <faustalexander@web.de>
2016-08-02 22:17:04 +0200

    Gui refactoring - startet

---
+++ Kundenverwaltung/Kundenverwaltung/src/Cust_Gui.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Cust_Gui {
+
+	    Button btn_speichernKunde = new Button("Datensatz speichern");
+	    Button btn_abbruchSpeichernKunde = new Button("Abbruch");
+	    Button btn_speichernBestellung = new Button("Bestellung speichern");
+	    Button btn_abbruchBestellung = new Button("Abbruch");
+	    Button btn_suchen = new Button("Kunde suchen");
+	    Button btn_neuerKunde = new Button("Neuer Kunde");
+	    Button btn_neueBestellung = new Button("Neue Bestellung");
+	    Button btn_kundeLoeschen = new Button("Kunde Loeschen");
+	    Button btn_DatenbankAus = new Button("Datenbankauswahl");
+	    Button btn_neueDatenbank = new Button("Neue Datenbank");
+	    Button btn_Ratenplan = new Button("Ratenplan als PDF");
+	    Button btn_RatenplanDruck = new Button("Ratenplan Drucken");
+	    Button btn_bestellungLoeschen = new Button("Bestellung Löschen");
+	    Button btn_bestellungAendern = new Button("Bestellung ändern");
+	    Button btn_RatenplanExcelExport = new Button("Excel Export");
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
